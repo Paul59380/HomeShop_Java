4dd87b50c38898c69f5dca15b3148e84b5639e96
Paul59380 <paul.genly59380@gmail.com>
2019-05-22 10:03:58 +0200

    Test branch dev , add construct in Product class

--- src/com/genly/homeshop/Product.java
+++ src/com/genly/homeshop/Product.java
@@ -5,6 +5,11 @@ public class Product {
     private String description;
     private double price;
 
+    public Product(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 
     public String getName() {
         return name;
