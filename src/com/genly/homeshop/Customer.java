d24fdc267a873d31689a5914fb8126add7779790
Paul59380 <paul.genly59380@gmail.com>
2019-05-22 09:57:09 +0200

    Create the constructor in class Bill, Customer and Product

--- src/com/genly/homeshop/Customer.java
+++ src/com/genly/homeshop/Customer.java
@@ -4,6 +4,11 @@ public class Customer {
     private String fullName;
     private String address;
 
+    public Customer(String fullName, String address) {
+        this.fullName = fullName;
+        this.address = address;
+    }
+
     public String getFullName() {
         return fullName;
     }
